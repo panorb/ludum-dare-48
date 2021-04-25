aad09c210000ecd1c93eca3dab3ca209e8b56400
Toni Schindler <toni.schindler@toschiwa.de>
2021-04-25 13:24:26 +0200

    Implemented command cat

--- godot/Commands/cat-command.gd
+++ godot/Commands/cat-command.gd
@@ -1,10 +1,38 @@
 extends "command.gd"
 
+export(NodePath) var file_system_node
+onready var file_system = get_node(file_system_node)
 
 func _ready():
 	aliases = ["cat"]
+	command_description = "Displays the content of a file"
 
-
-func execute(_args):
-	send_message("Not implemented.")
-	emit_signal("command_finished")
+func execute(args):
+	if args.size() >= 3:
+		throw_error("Error: Too many arguments")
+		execution_finished()
+		return
+	
+	if args.size() == 1 or args[1].empty():
+		throw_error("Error: Missing argument")
+		execution_finished()
+		return
+	
+	var path = ""
+	var absolute_path = ""
+	path = args[1]
+	path.replace("\\", "/")
+	absolute_path = file_system.to_absolute_path(path)
+	absolute_path = file_system.resolve_level_up_symbols(absolute_path)
+	
+	var node = file_system.get_filesystem_node(absolute_path)
+	if not node:
+		throw_error("Error: Invalid path")
+	elif not file_system.is_file(node):
+		throw_error("Error: No file")
+	elif not "content" in node:
+		throw_error("Error: Cannot read binary file")
+	else:
+		send_message(node.content)
+		
+	execution_finished()
