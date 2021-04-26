17c75e1b9a91883987fb7a946aa70518a7382cc7
Paul Norberger <panor@fastmail.com>
2021-04-26 12:21:43 +0200

    Added is_unlocked() for filesystem nodes

--- godot/FileSystem/node.gd
+++ godot/FileSystem/node.gd
@@ -1,7 +1,11 @@
 extends Node
 
 export(bool) var access_permission = true
-
+export(String) var password = ""
 
 func get_fs_name():
 	return name.replace('|', '.')
+
+
+func is_unlocked():
+	return access_permission and not password
